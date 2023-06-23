2ccf586bc6f69725c9d81061e5312bb376e28538
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-22 21:17:22 -0300

    Quadrante

--- Program.cs
+++ Program.cs
@@ -4,18 +4,22 @@ class Program {
   public static void Main (string[] args) {
     
                double x, y;
+               double A = 1°,B = 2°,C = 3°,D = 4°;
+               string Q = Quadrante;
+            Console.Clear();
+            Console.WriteLine();   
 
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("--- Quadrante Cartesiano ---\n");
             Console.ResetColor();
 
-            Console.WriteLine("--- Entre com o ponto desejado ----");
-            Console.Write("Entre com a Cordenada X...:");
+            Console.WriteLine("--- Fornecer as coordenadas X e Y do ponto desejado ----");
+            Console.Write("Entrar com a Cordenada X...:");
             x = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Entre com a Cordenada y...:");
+            Console.Write("Entrar com a Cordenada y...:");
             y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine();
-
+            
+            Console.ForegroundColor = ConsoleColor.Red;
             if (x == 0 && y == 0)
 
             {
@@ -25,13 +29,13 @@ class Program {
             else if (x > 0 && y > 0)
             {
 
-                Console.WriteLine($"O ponto ({x} , {y})  fica no quadrante 1.\n");               
+                Console.WriteLine($"O ponto ({x} , {y})  fica no 1º quadrante.\n");               
             }
 
             else if (x < 0 && y > 0)
 
             {
-                Console.WriteLine($"O ponto ({x} , {y})  fica no quadrante 2.\n");
+                Console.WriteLine($"O ponto ({x} , {y})  fica no 2º quadrante.\n");
 
             }
 
@@ -39,21 +43,41 @@ class Program {
 
             {
             
-                Console.WriteLine($"O ponto ({x} , {y})  fica no quadrante 3.\n");
+                Console.WriteLine($"O ponto ({x} , {y})  fica no 3° quadrante.\n");
             }
     
             else if (x > 0 && y < 0)
 
             {
-                Console.WriteLine($"O ponto ({x} , {y})  fica no quadrante 4.\n");
+                Console.WriteLine($"O ponto ({x} , {y})  fica no 4° quadrante.\n");
             }
             
             else
   
             {
-                Console.WriteLine($"O ponto ({x} , {y})  fica no eixo das cordenasdas.\n");
+                Console.WriteLine($"O ponto ({x} , {y})  fica no eixo das coordenadas.\n");
             }
 
             Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("             +Y              ");
+            Console.WriteLine("              |              ");
+            Console.WriteLine("      4º      |      1º      ");
+            Console.WriteLine("   Quadrante  |   Quadrante  ");
+            Console.WriteLine("              |              ");
+            Console.WriteLine("              |              ");
+            Console.WriteLine("-X------------0------------+X");
+            Console.WriteLine("              |              ");
+            Console.WriteLine("              |              ");
+            Console.WriteLine("      3°      |       2º     ");
+            Console.WriteLine("   Quadrante  |   Quadrante  ");
+            Console.WriteLine("              |              ");
+            Console.WriteLine("             -Y              ");
+            
+            Console.ResetColor();
+            
+            Console.WriteLine(Q);
         }
      }
